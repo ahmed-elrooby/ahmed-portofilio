2670ecbbfed40ffa66c9d7459846a8215ae65f8c
rooby <frontahmed099@gmail.com>
2024-12-25 00:36:45 +0200

    again

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,3 @@
-import dynamic from 'next/dynamic';
-
-// تعطيل SSR للمكون الذي قد يعتمد على document أو window
-const NotfounPage = dynamic(() => import('@/components/NotfoundPage/NotfoundPage'), { ssr: false });
 
 const Notfound = () => {
   return (
