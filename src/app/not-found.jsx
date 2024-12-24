f1851ece4c52731b2b93152a68e2abb097a49792
rooby <frontahmed099@gmail.com>
2024-12-25 00:39:03 +0200

    false

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,14 @@
+"use client"; // تأكد من أنها في أعلى الملف
+
+import dynamic from "next/dynamic";
+
+// تحميل المكون NotFoundPage بشكل ديناميكي وتعطيل SSR
+const NotFoundPage = dynamic(() => import('@/components/NotfoundPage/NotFoundPage'), { ssr: false });
 
 const Notfound = () => {
   return (
     <>
-      <NotfounPage />
+      <NotFoundPage />
     </>
   );
 };
